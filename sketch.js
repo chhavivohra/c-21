b11c6ba1bc15bdd4782d082f3aa0dd7a45487fe1
chhavivohra <88711944+chhavivohra@users.noreply.github.com>
2021-10-08 14:03:01 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,21 @@
+var fixedRect, movingRect;
+
+function setup() {
+  createCanvas(1200,800);
+  fixedRect = createSprite(600, 200, 50, 80);
+  fixedRect.shapeColor = "green";
+  fixedRect.debug = true;
+  movingRect = createSprite(400,200,80,30);
+  movingRect.shapeColor = "green";
+  movingRect.debug = true;
+  movingRect.velocityX=2;
+}
+
+function draw() {
+  background(0,0,0);  
+  if(isTouching(movingRect,fixedRect)){
+movingRect.velocityX=-2;
+
+  }
+  drawSprites();
+}
